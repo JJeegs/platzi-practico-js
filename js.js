18352ddf001f8f04e18dd82b61256c6be9a646f2
JJeegs <jhonnelmejor110@gmail.com>
2022-03-01 12:11:35 -0500

    Se crearon las funciones para calcular tanto el area como el perimetro de las figuras geometricas

--- js.js
+++ js.js
@@ -1,49 +1,31 @@
-var nombre = "Jhonn";
-var apellido = "Gutierrez"
-var nickname = "Jeegs";
-var edad = 17;
-var correo = "jhonnelmejor110@gmail.com"
-var mayor_edad = edad >= 18;
-var ahorros = 600000;
-var deudas = 20000;
-
-var completeName = nombre + " " + apellido;
-console.log(completeName);
-console.log(ahorros - deudas);
-
-function imprimirNombre(){
-    console.log("Mi nombre es " + completeName + ", pero prefiero que me digas " + nickname + ".");
+//perimetro del cuadrado
+function perimetroCuadrado(lado){
+    return lado * 4;
 }
 
-imprimirNombre();
-
-const tipoDeSuscripcion = "Basic";
-
-if (tipoDeSuscripcion=="Free"){
-    console.log("Solo puedes tomar los cursos gratis");
-}
-else if (tipoDeSuscripcion == "Basic"){
-    console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
-}
-else if (tipoDeSuscripcion == "Espert"){
-    console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
-}
-else if (tipoDeSuscripcion == "EspertPlus"){
-    console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+// area del cuadrado
+function areaCuadrado(lado){
+    return lado * lado;
 }
 
-i=0;
-while (i < 5){
-    console.log("El valor de i es: " + i);
-    i++
+//perimetro del triangulo
+function areaTriangulo(lado1, lado2, lado3){
+    return lado1 + lado2 + lado3;
 }
 
-
-var aguacate = ["delicioso", "nutritivo", "verde", "mi besto friento"];
-
-function imprimirTodo(array){
-    for (i = 0; i < array.length; i++)
-        console.log(array[i]);
+//area del triangulo
+function perimetroTriangulo(base, altura){
+    return base * altura / 2;
 }
 
-imprimirTodo(aguacate);
+//perimetro del circulo
+function perimetroCirculo(radio){
+    return 2 * Math.PI * radio;
+}
+
+//area del circulo
+function areaCirculo(radio){
+    return Math.PI * Math.pow(radio, 2); 
+}
+
+console.log(areaCirculo(4));
